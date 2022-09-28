1a4ae68eaf0d2359579020c1a73f12ab5028a65c
Diyar Taikenov <anbleidd@gmail.com>
2022-09-28 18:19:00 +0600

    initial commit

---
+++ core/src/main/kotlin/com/diyar/boids/Core.kt
@@ -0,0 +1,6 @@
+package com.diyar.boids
+
+import com.badlogic.gdx.ApplicationAdapter
+
+/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms. */
+class Core : ApplicationAdapter()
