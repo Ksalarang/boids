86e9646457e5cd8158581a33e79c2ca710ac8684
Diyar Taikenov <anbleidd@gmail.com>
2022-10-03 18:24:39 +0600

    add main menu

--- core/src/main/kotlin/com/diyar/boids/Core.kt
+++ core/src/main/kotlin/com/diyar/boids/Core.kt
@@ -1,6 +1,27 @@
 package com.diyar.boids
 
-import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Game
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
-/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms. */
-class Core : ApplicationAdapter()
+const val WIDTH = 1280f
+const val HEIGHT = 720f
+val BACKGROUND_COLOR: Color = Color.TEAL
+val TEXT_COLOR: Color = Color.WHITE
+
+class Core: Game() {
+    lateinit var batch: SpriteBatch
+    lateinit var font: BitmapFont
+
+    override fun create() {
+        batch = SpriteBatch()
+        font = BitmapFont()
+        setScreen(MainMenu(this))
+    }
+
+    override fun dispose() {
+        batch.dispose()
+        font.dispose()
+    }
+}
--- lwjgl3/src/main/kotlin/com/diyar/boids/lwjgl3/Lwjgl3Launcher.kt
+++ lwjgl3/src/main/kotlin/com/diyar/boids/lwjgl3/Lwjgl3Launcher.kt
@@ -5,12 +5,16 @@ package com.diyar.boids.lwjgl3
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 import com.diyar.boids.Core
+import com.diyar.boids.HEIGHT
+import com.diyar.boids.WIDTH
 
 /** Launches the desktop (LWJGL3) application. */
 fun main() {
     Lwjgl3Application(Core(), Lwjgl3ApplicationConfiguration().apply {
         setTitle("boids")
-        setWindowedMode(640, 480)
+        setWindowedMode(WIDTH.toInt(), HEIGHT.toInt())
+        setForegroundFPS(60)
+        useVsync(true)
         setWindowIcon(*(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
     })
 }
