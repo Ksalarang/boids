08c46e31f0590298f138b1cb160f995b374d9638
ksalarang <anbleidd@gmail.com>
2023-10-26 22:57:42 +0500

    fix: prevent all fish from crossing the walls

--- Assets/Scripts/GameScene/Systems/EvasionSystem.cs
+++ Assets/Scripts/GameScene/Systems/EvasionSystem.cs
@@ -10,18 +10,35 @@ public class EvasionSystem : System {
     readonly Predator predator;
     readonly PredatorSettings predatorSettings;
     readonly BoidSettings boidSettings;
-    readonly Vector3 bottomLeft;
-    readonly Vector3 topRight;
-    readonly float boidHalfSize;
+    readonly float boidSize;
+    readonly float predatorSize;
+    // camera rectangle corners
+    Vector3 boidBottomLeft;
+    Vector3 boidTopRight;
+    Vector3 predatorBottomLeft;
+    Vector3 predatorTopRight;
 
     public EvasionSystem(Boid[] boids, Predator predator, GameSettings settings, Camera camera) {
         this.boids = boids;
         this.predator = predator;
         predatorSettings = settings.predatorSettings;
         boidSettings = settings.boidSettings;
-        bottomLeft = camera.getBottomLeft();
-        topRight = camera.getTopRight();
-        boidHalfSize = boids[0].transform.localScale.x / 2;
+        boidSize = boids[0].transform.localScale.x;
+        predatorSize = predator.transform.localScale.x;
+        setWallBorders(camera, predatorSize / 3);
+    }
+
+    void setWallBorders(Camera camera, float offset) {
+        boidBottomLeft = predatorBottomLeft = camera.getBottomLeft();
+        boidTopRight = predatorTopRight = camera.getTopRight();
+        boidBottomLeft.x += offset;
+        boidBottomLeft.y += offset;
+        boidTopRight.x -= offset;
+        boidTopRight.y -= offset;
+        predatorBottomLeft.x += offset;
+        predatorBottomLeft.y += offset;
+        predatorTopRight.x -= offset;
+        predatorTopRight.y -= offset;
     }
 
     public void update(float deltaTime) {
@@ -29,24 +46,30 @@ public class EvasionSystem : System {
         var wallAvoidanceEnabled = boidSettings.wallAvoidanceEnabled;
         var predatorEvasionEnabled = boidSettings.predatorEvasionEnabled;
         var wallAvoidanceForce = boidSettings.wallAvoidanceForce;
+        var boidBottomLeft = this.boidBottomLeft;
+        var boidTopRight = this.boidTopRight;
+        var boidSize = this.boidSize;
         foreach (var boid in boids) {
-            if (wallAvoidanceEnabled) avoidWalls(boid.transform, deltaTime, wallAvoidanceForce);
-            if (predatorEvasionEnabled) evadePredator(boid, predatorPosition, deltaTime);
+            if (wallAvoidanceEnabled) {
+                avoidWalls(boid.transform, boidSize, deltaTime, wallAvoidanceForce, boidBottomLeft, boidTopRight);
+            }
+            if (predatorEvasionEnabled) {
+                evadePredator(boid, predatorPosition, deltaTime);
+            }
         }
-        avoidWalls(predator.transform, deltaTime, predatorSettings.wallAvoidanceForce);
+        avoidWalls(predator.transform, predatorSize, deltaTime, predatorSettings.wallAvoidanceForce, predatorBottomLeft, predatorTopRight);
     }
 
-    void avoidWalls(Transform transform, float deltaTime, float avoidanceForce) {
+    void avoidWalls(Transform transform, float bodySize, float deltaTime, float avoidanceForce, Vector3 bottomLeft, Vector3 topRight) {
         var currentAngle = transform.eulerAngles.z;
         if (currentAngle < 0) currentAngle += 360;
         var newAngle = currentAngle;
-        var frontPosition = transform.position + boidHalfSize * MathUtils.angleToVector(currentAngle);
+        var frontPosition = transform.position + 0.5f * bodySize * MathUtils.angleToVector(currentAngle);
         const int maxRayCount = 18;
         const float angleStep = 10f;
-        const int stepCount = 10;
-        const float stepLength = 0.2f;
-        var bottomLeft = this.bottomLeft;
-        var topRight = this.topRight;
+        const float stepLengthFraction = 0.2f;
+        var stepLength = bodySize * stepLengthFraction;
+        var stepCount = bodySize / stepLength;
         // cast rays to determine clear path
         for (var i = 0; i < maxRayCount; i++) {
             var sign = i % 2 == 0 ? -1f : 1f;
