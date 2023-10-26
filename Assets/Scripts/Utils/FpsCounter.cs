cb71d09771a556ce4f7ef49078d092e14b5f5c35
ksalarang <anbleidd@gmail.com>
2023-10-26 21:36:24 +0500

    add fps counter

---
+++ Assets/Scripts/Utils/FpsCounter.cs
@@ -0,0 +1,33 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Utils {
+[RequireComponent(typeof(TMP_Text))]
+public class FpsCounter : MonoBehaviour {
+    [SerializeField] [Range(0f, 1f)] float expSmoothingFactor = 0.9f;
+    [SerializeField] float refreshFrequency = 0.4f;
+
+    float timeSinceUpdate;
+    float averageFps = 1f;
+    TMP_Text label;
+
+    void Start() {
+        label = GetComponent<TMP_Text>();
+    }
+
+    void Update() {
+        // Exponentially weighted moving average (EWMA)
+        averageFps = expSmoothingFactor * averageFps + (1f - expSmoothingFactor) * 1f / Time.unscaledDeltaTime;
+
+        if (timeSinceUpdate < refreshFrequency) {
+            timeSinceUpdate += Time.deltaTime;
+            return;
+        }
+
+        var fps = Mathf.RoundToInt(averageFps);
+        label.text = fps.ToString();
+
+        timeSinceUpdate = 0f;
+    }
+}
+}
