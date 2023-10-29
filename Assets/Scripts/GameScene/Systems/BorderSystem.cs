201cda0d379cd5e11761179baa23272198629fc4
ksalarang <anbleidd@gmail.com>
2023-10-29 15:30:00 +0500

    refactor: rename fields

--- Assets/Scripts/GameScene/Systems/BorderSystem.cs
+++ Assets/Scripts/GameScene/Systems/BorderSystem.cs
@@ -6,45 +6,49 @@ public class BorderSystem : System {
     readonly Predator predator;
     readonly Vector3 bottomLeft;
     readonly Vector3 topRight;
-    readonly float halfSize;
-    readonly float predatorHalfSize;
+    readonly float boidOffset;
+    readonly float predatorOffset;
 
     public BorderSystem(Boid[] boids, Predator predator, Vector3 bottomLeft, Vector3 topRight, float boidSize) {
         this.boids = boids;
         this.predator = predator;
         this.bottomLeft = bottomLeft;
         this.topRight = topRight;
-        halfSize = boidSize / 2;
-        predatorHalfSize = predator.transform.localScale.x / 2;
+        boidOffset = boidSize / 2;
+        predatorOffset = predator.transform.localScale.x / 2;
     }
 
     public void update(float deltaTime) {
+        updateBoids();
+        updatePredator();
+    }
+
+    void updateBoids() {
         foreach (var boid in boids) {
             var position = boid.transform.position;
-            if (position.x + halfSize < bottomLeft.x) {
+            if (position.x + boidOffset < bottomLeft.x) {
                 boid.transform.position = new Vector3(topRight.x, position.y, position.z);
-            } else if (position.x - halfSize > topRight.x) {
+            } else if (position.x - boidOffset > topRight.x) {
                 boid.transform.position = new Vector3(bottomLeft.x, position.y, position.z);
             }
-            if (position.y + halfSize < bottomLeft.y) {
+            if (position.y + boidOffset < bottomLeft.y) {
                 boid.transform.position = new Vector3(position.x, topRight.y, position.z);
-            } else if (position.y - halfSize > topRight.y) {
+            } else if (position.y - boidOffset > topRight.y) {
                 boid.transform.position = new Vector3(position.x, bottomLeft.y, position.z);
             }
         }
-        updatePredator();
     }
 
     void updatePredator() {
         var position = predator.transform.position;
-        if (position.x + predatorHalfSize < bottomLeft.x) {
+        if (position.x + predatorOffset < bottomLeft.x) {
             predator.transform.position = new Vector3(topRight.x, position.y, position.z);
-        } else if (position.x - predatorHalfSize > topRight.x) {
+        } else if (position.x - predatorOffset > topRight.x) {
             predator.transform.position = new Vector3(bottomLeft.x, position.y, position.z);
         }
-        if (position.y + predatorHalfSize < bottomLeft.y) {
+        if (position.y + predatorOffset < bottomLeft.y) {
             predator.transform.position = new Vector3(position.x, topRight.y, position.z);
-        } else if (position.y - predatorHalfSize > topRight.y) {
+        } else if (position.y - predatorOffset > topRight.y) {
             predator.transform.position = new Vector3(position.x, bottomLeft.y, position.z);
         }
     }
